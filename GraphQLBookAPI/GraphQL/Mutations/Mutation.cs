aee6e124c66bcbc3509763a8f1df6a7c2ce98c4e
Yossathorn <yossatron@gmail.com>
2025-03-06 08:12:44 +0700

    เพิ่ม bookMutation สำหรับ create update delete

--- GraphQLBookAPI/GraphQL/Mutations/Mutation.cs
+++ GraphQLBookAPI/GraphQL/Mutations/Mutation.cs
@@ -3,5 +3,6 @@ namespace GraphQLBooksAPI.GraphQL.Mutations;
 public class Mutation
 {
     public AuthorMutation AuthorMutation => new(); // ✅ เพิ่ม AuthorMutation
+    public BookMutation BookMutation => new(); // ✅ เพิ่ม BookMutation
     public ChatMutation ChatMutation => new(); // ✅ เพิ่ม ChatMutation
 }
